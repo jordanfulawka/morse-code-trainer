a4eeb33d025e9e6cae96340a445818f8fe5fd2d5
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-01-14 22:41:16 -0500

    implementing timing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
+
 function App() {
-  return <div>Hello world!</div>;
+  const pressStart = useRef(0);
+  const [output, setOutput] = useState('');
+
+  useEffect(() => {
+    const onKeyDown = (e) => {
+      if (e.code === 'Space') {
+        pressStart.current = performance.now();
+      }
+    };
+
+    const onKeyUp = (e) => {
+      if (e.code === 'Space') {
+        const duration = performance.now() - pressStart.current;
+        const symbol = duration < 200 ? '.' : '-';
+        setOutput((prev) => prev + symbol);
+      }
+    };
+
+    window.addEventListener('keydown', onKeyDown);
+    window.addEventListener('keyup', onKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+      window.removeEventListener('keyup', onKeyUp);
+    };
+  }, []);
+
+  return (
+    <div className='flex flex-col items-center'>
+      <h1>hello</h1>
+      <p>Hold spacebar to input Morse</p>
+      <div>{output}</div>
+    </div>
+  );
 }
 
 export default App;
